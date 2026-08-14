54929ab2b310b26294a423f4ec4454b2f580bd40
Daniel Olugbenga <danielgebga11@gmail.com>
2026-08-14 16:48:15 +0100

    fix: update projects data index exports

--- data/index.ts
+++ data/index.ts
@@ -1,3 +1,9 @@
 export { default as education } from "@/data/education";
-export { softwareSkills, architecturalSkills, experience, leadership } from "@/data/skills";
+export {
+  softwareSkills,
+  architecturalSkills,
+  experience,
+  leadership,
+} from "@/data/skills";
 export { default as projects } from "@/data/projects";
+export type { Project } from "@/data/projects";
